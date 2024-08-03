faecab01b378da509c97eee24616957b4cb2e3cf
Muhammad Zahid Imran <33024798+zahidappsgenii@users.noreply.github.com>
2024-08-04 02:42:32 +0500

    updates

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "CircleProgressView",
+    platforms: [.iOS(.v8)],
+    products: [
+        .library(
+            name: "CircleProgressView",
+            targets: ["CircleProgressView"]),
+    ],
+    targets: [
+        .target(
+            name: "CircleProgressView",
+            path: "Source/ProgressView",
+            publicHeadersPath: "ObjC"),
+    ],
+    swiftLanguageVersions: [.v4_2]
+)
